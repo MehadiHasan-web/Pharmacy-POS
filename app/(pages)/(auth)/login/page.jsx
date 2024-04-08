05df3e82c5679425f998e52f3f482b4ab93aec6e
MH Shakil <mhshakil06@gmail.com>
2024-04-08 12:38:18 +0600

    auth,middelward add

---
+++ app/(pages)/(auth)/login/page.jsx
@@ -0,0 +1,21 @@
+
+export default function LoginPage() {
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form >
+                <input
+                    type="text"
+                    placeholder="Username"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    );
+}
